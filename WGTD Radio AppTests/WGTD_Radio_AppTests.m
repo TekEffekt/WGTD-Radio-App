6d962e34e6a3065006d7ae1d8f012f26869679a9
Kyle K Zawacki <z95sk8@outlook.com>
2015-03-22 21:24:13 -0500

    Initial Commit

---
+++ WGTD Radio AppTests/WGTD_Radio_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  WGTD_Radio_AppTests.m
+//  WGTD Radio AppTests
+//
+//  Created by Kyle Zawacki on 3/22/15.
+//  Copyright (c) 2015 University Of Wiscosnin Parkside. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WGTD_Radio_AppTests : XCTestCase
+
+@end
+
+@implementation WGTD_Radio_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
