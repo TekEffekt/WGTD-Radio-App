1c86b6970b3febd0af616bcb67ccf3bc2d526700
Kyle K Zawacki <z95sk8@outlook.com>
2015-05-22 15:47:57 -0500

    Revert 71c1d3d..db7d036
    This rolls back to commit 71c1d3d07d8f5fb4c837f15b78174c367bfb0212.

--- WGTD Radio App/ViewController.m
+++ WGTD Radio App/ViewController.m
@@ -10,9 +10,6 @@
 #import "STKAudioPlayer.h"
 #import "WaveView.h"
 #import "InfoViewController.h"
-#import <AVFoundation/AVFoundation.h>
-#import <MediaPlayer/MPNowPlayingInfoCenter.h>
-#import <MediaPlayer/MPMediaItem.h>
 
 @interface ViewController () <STKAudioPlayerDelegate>
 
@@ -30,10 +27,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *bannerImage;
 
 @property(nonatomic) BOOL playing;
-@property(strong, nonatomic) STKAudioPlayer *currentAudioPlayer;
-@property(strong, nonatomic) STKAudioPlayer *jazzPlayer;
-@property(strong, nonatomic) STKAudioPlayer *classicalPlayer;
-@property(strong, nonatomic) STKAudioPlayer *readingPlayer;
+@property(strong, nonatomic) STKAudioPlayer *audioPlayer;
 
 @property(strong, nonatomic) NSArray *channels;
 @property(nonatomic) int channelIndex;
@@ -51,36 +45,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.classicalPlayer = [[STKAudioPlayer alloc] init];
-    [self.classicalPlayer play:@"http://media.gtc.edu:8000/stream"];
-    self.classicalPlayer.muted = YES;
-    self.classicalPlayer.delegate = self;
-    self.classicalPlayer.meteringEnabled = YES;
-    
-    self.jazzPlayer = [[STKAudioPlayer alloc] init];
-    [self.jazzPlayer play:@"http://199.255.3.11:88/broadwave.mp3?src=1&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Fhd2.asp"];
-    self.jazzPlayer.muted = YES;
-    self.jazzPlayer.delegate = self;
-    self.jazzPlayer.meteringEnabled = YES;
-    
-    self.readingPlayer = [[STKAudioPlayer alloc] init];
-    [self.readingPlayer play:@"http://199.255.3.11:88/broadwave.mp3?src=4&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Freading.asp"];
-    self.readingPlayer.muted = YES;
-    self.readingPlayer.delegate = self;
-    self.readingPlayer.meteringEnabled = YES;
-    
-    self.currentAudioPlayer = self.classicalPlayer;
-    
-    
+    self.audioPlayer = [[STKAudioPlayer alloc] init];
+    self.audioPlayer.delegate = self;
+    self.audioPlayer.meteringEnabled = YES;
     [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(updateWaveView) userInfo:nil repeats:YES];
     [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(changeAdBanner) userInfo:nil repeats:YES];
     
-//    self.channels = @[@"http://media.gtc.edu:8000/stream", @"http://199.255.3.11:88/broadwave.mp3?src=1&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Fhd2.asp",
-//                      @"http://199.255.3.11:88/broadwave.mp3?src=4&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Freading.asp"];
-    
-    self.channels = @[self.classicalPlayer, self.jazzPlayer, self.readingPlayer];
-    self.channelLabelTexts = @[@"Classical", @"Jazz", @"Reading Service"];
-    self.skipButtonLabelText = @[@"Classical", @"Jazz", @"Reading"];
+    self.channels = @[@"http://media.gtc.edu:8000/stream", @"http://199.255.3.11:88/broadwave.mp3?src=1&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Fhd2.asp",
+                      @"http://199.255.3.11:88/broadwave.mp3?src=2&rate=1&ref=http%3A%2F%2Fwww.wgtd.org%2Fhd3.asp"];
+    self.channelLabelTexts = @[@"Classical Radio", @"Jazz Radio", @"Community Radio"];
+    self.skipButtonLabelText = @[@"Classical", @"Jazz", @"Community"];
     
     if(![[NSUserDefaults standardUserDefaults] integerForKey:@"Channel Index"])
     {
@@ -118,25 +92,6 @@
     self.playButton.tintColor = self.view.tintColor;
     
     self.channelLabel.text = self.channelLabelTexts[self.channelIndex];
-    
-    // Code to respond to Control Center events
-    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
-    [self becomeFirstResponder];
-    
-    NSError *setCategoryError = nil;
-    NSError *activationError = nil;
-    
-    [[AVAudioSession sharedInstance] setActive:YES error:&activationError];
-    [[AVAudioSession sharedInstance] setDelegate:self];
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    
-    //End recieving events
-    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
-    [self resignFirstResponder];
 }
 
 - (void)viewDidLayoutSubviews
@@ -162,7 +117,7 @@
 {
     if(!self.playing)
     {
-        self.currentAudioPlayer.muted = NO;
+        [self.audioPlayer play:self.channels[self.channelIndex]];
         UIImage *image = [UIImage imageNamed:@"pause"];
         image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
         [self.playButton setImage:image forState:UIControlStateNormal];
@@ -170,15 +125,13 @@
         self.playing = YES;
     } else
     {
-        self.currentAudioPlayer.muted = YES;
+        [self.audioPlayer stop];
         UIImage *image = [UIImage imageNamed:@"play"];
         image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
         [self.playButton setImage:image forState:UIControlStateNormal];
         
         self.playing = NO;
     }
-    
-    [self updateNowPlayingInfo];
 }
 
 - (IBAction)skipButtonsPressed:(UIButton *)sender
@@ -201,15 +154,16 @@
         }
     }
     
+    if(self.playing)
+    {
+        [self.audioPlayer stop];
+        [self.audioPlayer play:self.channels[self.channelIndex]];
+    }
+    
     [[NSUserDefaults standardUserDefaults] setInteger:self.channelIndex forKey:@"Channel Index"];
     self.channelLabel.text = self.channelLabelTexts[self.channelIndex];
     
     [self setSkipButtonLabels];
-    [self updateNowPlayingInfo];
-    
-    self.currentAudioPlayer.muted = YES;
-    self.currentAudioPlayer = self.channels[self.channelIndex];
-    self.currentAudioPlayer.muted = NO;
 }
 
 - (void)setSkipButtonLabels
@@ -247,7 +201,8 @@
 # pragma mark - Wave View
 - (void)updateWaveView
 {
-    CGFloat level = (([self.currentAudioPlayer averagePowerInDecibelsForChannel:1] + 60) / 60);
+    CGFloat level = (([self.audioPlayer averagePowerInDecibelsForChannel:1] + 60) / 60);
+    NSLog(@"%f", level);
     
     [self.waveView updateWithLevel:level];
 }
@@ -255,13 +210,11 @@
 #pragma mark - Audio Player Delegate
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer stateChanged:(STKAudioPlayerState)state previousState:(STKAudioPlayerState)previousState
 {
-    NSLog(@"Current State: %u", state);
-    NSLog(@"Previous State: %u", previousState);
+    
 }
 
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer unexpectedError:(STKAudioPlayerErrorCode)errorCode
 {
-    NSLog(@"Error Code: %u", errorCode);
 }
 
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer didStartPlayingQueueItemId:(NSObject*)queueItemId
@@ -278,77 +231,6 @@
 
 -(void) audioPlayer:(STKAudioPlayer *)audioPlayer logInfo:(NSString *)line
 {
-}
-
-#pragma mark - Remote Control
-
-- (BOOL)canBecomeFirstResponder {
-    return YES;
-}
-
-- (void)remoteControlReceivedWithEvent:(UIEvent *)receivedEvent {
-    
-    if (receivedEvent.type == UIEventTypeRemoteControl) {
-        switch (receivedEvent.subtype) {
-            case UIEventSubtypeRemoteControlPlay:
-                self.currentAudioPlayer.muted = NO;
-                break;
-            case UIEventSubtypeRemoteControlPause:
-                self.currentAudioPlayer.muted = YES;
-                break;
-            case UIEventSubtypeRemoteControlTogglePlayPause:
-                if (self.currentAudioPlayer.state == STKAudioPlayerStatePlaying) {
-                    self.currentAudioPlayer.muted = YES;
-                }
-                else {
-                    self.currentAudioPlayer.muted = NO;
-                }
-                break;
-                
-            case UIEventSubtypeRemoteControlNextTrack:
-                self.channelIndex += 1;
-                
-                if(self.channelIndex > 2)
-                {
-                    self.channelIndex = 0;
-                }
-                self.currentAudioPlayer.muted = YES;
-                self.currentAudioPlayer = self.channels[self.channelIndex];
-                self.currentAudioPlayer.muted = NO;
-                [[NSUserDefaults standardUserDefaults] setInteger:self.channelIndex forKey:@"Channel Index"];
-                self.channelLabel.text = self.channelLabelTexts[self.channelIndex];
-                
-                [self setSkipButtonLabels];
-                [self updateNowPlayingInfo];
-                break;
-            case UIEventSubtypeRemoteControlPreviousTrack:
-                self.channelIndex -= 1;
-                
-                if(self.channelIndex < 0)
-                {
-                    self.channelIndex = 2;
-                }
-                
-                self.currentAudioPlayer.muted = YES;
-                self.currentAudioPlayer = self.channels[self.channelIndex];
-                self.currentAudioPlayer.muted = NO;
-                [[NSUserDefaults standardUserDefaults] setInteger:self.channelIndex forKey:@"Channel Index"];
-                self.channelLabel.text = self.channelLabelTexts[self.channelIndex];
-                
-                [self setSkipButtonLabels];
-                [self updateNowPlayingInfo];
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-- (void)updateNowPlayingInfo
-{
-    NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
-    [songInfo setObject:self.channelLabelTexts[self.channelIndex] forKey:MPMediaItemPropertyTitle];
-    [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:songInfo];
 }
 
 @end
